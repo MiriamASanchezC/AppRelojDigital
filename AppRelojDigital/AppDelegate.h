9b159b209377cdeb436c8491b57ec0a8343d372d
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-27 17:18:42 -0600

    Initial Commit

---
+++ AppRelojDigital/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppRelojDigital
+//
+//  Created by Miriam Sanchez on 27/02/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AppRelojDigital/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AppRelojDigital
+//
+//  Created by Miriam Sanchez on 27/02/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
