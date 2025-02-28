f567cace8cbfd45603637c99d3545491bebbfdc4
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-27 18:01:55 -0600

    App reloj digital

--- AppRelojDigital/ViewController.h
+++ AppRelojDigital/ViewController.h
@@ -7,7 +7,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSTimer *timer;
+}
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 
 @end
--- AppRelojDigital/ViewController.m
+++ AppRelojDigital/ViewController.m
@@ -16,6 +16,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+}
+
+-(void)updateTimer{
+    NSDateFormatter *timeFormat = [[NSDateFormatter alloc]init];
+    [timeFormat setDateFormat:@"HH:mm:ss"];
+    self.label.text=[timeFormat stringFromDate:[NSDate date]];
 }
 
 
