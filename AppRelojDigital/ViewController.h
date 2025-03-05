871cb73056b84fa97ca7cf21a06610996fba5772
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-03-04 18:20:09 -0600

    cambioss

--- AppRelojDigital/ViewController.h
+++ AppRelojDigital/ViewController.h
@@ -11,6 +11,14 @@
     NSTimer *timer;
 }
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImage;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *clockColorSeg;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *backgroundSeg;
+@property (weak, nonatomic) IBOutlet UIButton *settingsButton;
+- (IBAction)clockColor:(id)sender;
+- (IBAction)backgroundSegCtrl:(id)sender;
+- (IBAction)settings:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *settingsView;
 
 
 @end
--- AppRelojDigital/ViewController.m
+++ AppRelojDigital/ViewController.m
@@ -17,6 +17,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+    self.settingsView.hidden = YES;
+    self.settingsButton.alpha = 0.20;
 }
 
 -(void)updateTimer{
@@ -26,4 +28,46 @@
 }
 
 
+- (IBAction)settings:(id)sender {
+    if(self.settingsView.hidden == YES){
+        self.settingsView.hidden = NO;
+        self.settingsButton.alpha = 1.0;
+        [self.settingsButton setTitle:@"Hide settings" forState:UIControlStateNormal];
+    }else{
+        self.settingsView.hidden = YES;
+        self.settingsButton.alpha = 0.15;
+        [self.settingsButton setTitle:@"Show settings" forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)backgroundSegCtrl:(id)sender {
+    /*
+    if (self.backgroundSeg.selectedSegmentIndex == 0) {
+        self.backgroundImage.backgroundColor = [UIColor blackColor];
+    }else  if (self.backgroundSeg.selectedSegmentIndex == 1) {
+        self.backgroundImage.backgroundColor = [UIColor greenColor];
+    }else  if (self.backgroundSeg.selectedSegmentIndex == 2) {
+        self.backgroundImage.backgroundColor = [UIColor systemPinkColor];
+    }
+    */
+    
+    //Para cambiar el background de una imagen
+    if (self.backgroundSeg.selectedSegmentIndex == 0) {
+        self.backgroundImage.image = [UIImage imageNamed:@"Background1"];
+    }else if (self.backgroundSeg.selectedSegmentIndex == 1) {
+        self.backgroundImage.image = [UIImage imageNamed:@"Background2"];
+    }else if (self.backgroundSeg.selectedSegmentIndex == 2) {
+        self.backgroundImage.image = [UIImage imageNamed:@"Background4"];
+    }
+}
+
+- (IBAction)clockColor:(id)sender {
+    if (self.clockColorSeg.selectedSegmentIndex == 0) {
+        self.label.textColor = [UIColor whiteColor];
+    }else if(self.clockColorSeg.selectedSegmentIndex == 1){
+        self.label.textColor = [UIColor yellowColor];
+    }else if(self.clockColorSeg.selectedSegmentIndex == 2){
+        self.label.textColor = [UIColor redColor];
+    }
+}
 @end
